a2b8e1b4f7120c5dc0daf8db7ffeb79e4fa411d2
Andreas Larsen <andreas@larsenwork.com>
2015-12-21 16:26:01 +0100

    layout fix

--- site/snippets/bags.php
+++ site/snippets/bags.php
@@ -4,7 +4,7 @@
       <h3><?php echo page('bags')->title() ?></h3>
     </div>
     <?php foreach (page('bags')->images() as $item): ?>
-      <div class="small-6 large-4 columns">
+      <div class="medium-6 large-4 columns">
         <img src="<?php echo $item->url() ?>" alt="<?php echo $item->title()->html() ?>" >
         <h5><?php echo $item->caption()->html() ?></h5>
         <p><?php echo $item->description() ?></p>
--- site/snippets/intro.php
+++ site/snippets/intro.php
@@ -1,7 +1,7 @@
 <div class="section" id="intro">
   <div class="row">
     <?php foreach (page('intro')->images() as $item): ?>
-      <div class="small-6 large-4 columns">
+      <div class="medium-6 large-4 columns">
         <img src="<?php echo $item->url() ?>" alt="<?php echo $item->title()->html() ?>" >
         <h5><?php echo $item->caption()->html() ?></h5>
         <p><?php echo $item->description() ?></p>
