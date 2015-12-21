f457aa45645acad4ac9622fcea12156907e3cb94
Viktor Shamal Andersen <Sphinxominator@gmail.com>
2015-12-21 15:53:24 +0100

    added intro and about

--- site/snippets/about.php
+++ site/snippets/about.php
@@ -1,7 +1,11 @@
-<section>
-    <div class="large-8">
-      <img src="<?php echo page('about')->images()->first()->url() ?>" alt="<?php echo page('about')->title()->html() ?>" >
+<div class="section" id = "about">
+  <div class="row">
+    <div class="large-6 columns">
       <h3><?php echo page('about')->title()->html() ?></h3>
       <p><?php echo page('about')->text() ?></p>
     </div>
-</section>
+    <div class="large-6 columns">
+      <img src="<?php echo page('about')->images()->first()->url() ?>" alt="<?php echo page('about')->title()->html() ?>" >
+    </div>
+  </div>
+</div>
