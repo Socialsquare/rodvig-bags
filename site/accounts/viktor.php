1127f54f1d95374bf87a1b43450b7378fdb717a1
Viktor Shamal Andersen <Sphinxominator@gmail.com>
2015-12-21 09:52:04 +0100

    initial commit

---
+++ site/accounts/viktor.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: viktor
+email: sphinxominator@gmail.com
+password: >
+  $2a$10$670acqNwJ6eaLhcWfsSkjeCcZvIWgK.WdZbLW7d6X.d3tYCe8Utbm
+language: en
+role: admin
