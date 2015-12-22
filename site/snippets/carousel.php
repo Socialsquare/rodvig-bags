4dcc71bd2fa9f3761dde2610d8ffc28218a9a7af
Andreas Larsen <andreas@larsenwork.com>
2015-12-22 11:07:48 +0100

    more cleanup

--- site/snippets/carousel.php
+++ site/snippets/carousel.php
@@ -1,13 +1,11 @@
 <div class="owl-carousel">
   <?php foreach (page('carousel')->images()->sortBy('sort', 'asc') as $item): ?>
     <div class="item" style="background-image: url('<?php echo $item->url()?>')">
-      <div class="owl-text">
-        <div class="row">
-          <div class="columns">
-            <h1><?php echo $item->caption() ?></h1>
-            <div></div>
-            <p><?php echo $item->description() ?></p>
-          </div>
+      <div class="row">
+        <div class="columns">
+          <h1><?php echo $item->caption() ?></h1>
+          <div></div>
+          <p><?php echo $item->description() ?></p>
         </div>
       </div>
     </div>
